3fade8b40b36db19d8051fc82693b1a597c88f23
milad <seyeadmiladnouri@gmail.com>
2026-06-19 20:43:42 +0330

    Run on the port

--- Flask_app.py
+++ Flask_app.py
@@ -1,16 +1,33 @@
 from flask import Flask, request, jsonify
 import pickle
+from nltk.corpus import stopwords
 
 app = Flask(__name__)
+
+def text_process(mess: str):
+    import string
+
+    mess = mess.lower()
+
+    nopuct = [char for char in mess if char not in string.punctuation]
+    nopuct = ''.join(nopuct)
+
+    return [
+        word
+        for word in nopuct.split()
+        if word.lower() not in stopwords.words('english')
+    ]
 
 with open('spam_detector.pkl', 'rb') as model_file:
     pipeline = pickle.load(model_file)
 
+
 @app.route('/')
 def home():
     return {
-"message": "Spam Detection API is Running"
-}
+        "message": "Spam Detection API is Running"
+    }
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -20,16 +37,15 @@ def predict():
     text_message = data.get("message", "")
 
     prediction = pipeline.predict([text_message])[0]
-
     probability = pipeline.predict_proba([text_message])[0]
 
     return jsonify({
-    "message": text_message,
-    "prediction": prediction,
-    "ham_probability": float(probability[0]),
-    "spam_probability": float(probability[1])
-})
+        "message": text_message,
+        "prediction": prediction,
+        "ham_probability": float(probability[0]),
+        "spam_probability": float(probability[1])
+    })
 
-    if name == "main":
-        app.run(debug=True)
 
+if __name__ == "__main__":
+    app.run(debug=True)
